cb29e2f203d1da2a6008b6f3724d1171bfec6ad9
Jamie Hoyle <jamie@mirrorweb.com>
2020-01-16 17:42:23 +0000

    Updated comparison format

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,19 +3,19 @@
 sonar_args=("-Dsonar.login=$INPUT_LOGIN" "-Dsonar.host.url=$INPUT_URL" "-Dsonar.projectBaseDir=$INPUT_BASEDIR" "-Dsonar.projectKey=$INPUT_APP" "-Dsonar.projectName=$INPUT_APP" "-Dsonar.scm.provider=git" "-Dsonar.sourceEncoding=UTF-8")
 
 # check for a branch name
-if "$INPUT_BRANCHNAME" !== "" 
+if [ -v "$INPUT_BRANCHNAME" ] 
   then 
     sonar_args=("${sonar_args[@]}" -Dsonar.branch.name) 
 fi
 
 # check for PR details
-if "$INPUT_PULLREQUESTKEY" !== "" && "$INPUT_PULLREQUESTBRANCH" !== ""
+if [ -v "$INPUT_PULLREQUESTKEY" ] && [ -v "$INPUT_PULLREQUESTBRANCH" ]
   then
     sonar_args=("${sonar_args[@]}" "-Dsonar.pullrequest.key=$INPUT_PULLREQUESTKEY" "-Dsonar.pullrequest.key=$INPUT_PULLREQUESTBRANCH")
 fi
 
 # finally, check for a pull request base if we have one
-if "$INPUT_PULLREQUESTBASE" !== ""
+if [ -v "$INPUT_PULLREQUESTBASE" ]
   then
       sonar_args=("${sonar_args[@]}" "-Dsonar.pullrequest.base=$INPUT_PULLREQUESTBASE")
 fi
